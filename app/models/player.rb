fddc84e17c1bf5cc714943e4fb86577727265826
Greg Towne <townegr@yahoo.com>
2014-11-15 12:09:08 -0500

    updated seeds.rb

--- app/models/player.rb
+++ app/models/player.rb
@@ -5,20 +5,4 @@ class Player < ActiveRecord::Base
   has_many :statuses
   has_many :reports, through: :statuses, dependent: :destroy
   belongs_to :team
-
-  def self.retrieve_player_data
-    Team.all.each do |team|
-      SportsDataApi::Nfl.team_roster(team.abbr_name).players.each do |p|
-        unless p.player == nil
-          name = p.player[:name_full]
-          position = p.player[:position]
-          player_code = p.player[:id]
-          team_id = team.id
-
-          Player.create(name: name, position: position, player_code: player_code, team_id: team_id)
-        end
-      end
-      sleep(1)
-    end
-  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,26 @@
+# activate API key
 SportsDataApi.set_key(:nfl, Rails.application.secrets[:sports_data_api][:key])
+puts "Sports Data API activated"
 
+# build NFL teams
 SportsDataApi::Nfl.teams.each do |team|
   Team.create!(abbr_name: team.id, market: team.market, name: team.name)
+end
+puts "NFL teams seeded"
+sleep(1)
+
+# create all NFL player profiles
+Team.all.each do |team|
+  SportsDataApi::Nfl.team_roster(team.abbr_name).players.each do |p|
+    unless p.player == nil
+      name = p.player[:name_full]
+      position = p.player[:position]
+      player_code = p.player[:id]
+      team_id = team.id
+
+      Player.create(name: name, position: position, player_code: player_code, team_id: team_id)
+    end
+  end
+  puts "#{team.name} players seeded"
+  sleep(1)
 end
