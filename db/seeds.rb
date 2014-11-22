1ffca2cc8519d26d7b3350bc7f8122402ffbc82c
Greg Towne <townegr@yahoo.com>
2014-11-22 10:57:51 -0500

    add simple_form plus other gems

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,28 @@
+# ===============================================================================
+# seed with id restarting with 1
+
 ["players", "teams", "schedules", "weeks", "games", "stats", "reports", "statuses"].each do |table|
   ActiveRecord::Base.connection.execute("ALTER SEQUENCE #{table}_id_seq RESTART WITH 1")
 end
 
+# ===============================================================================
 # activate API key
+
 SportsDataApi.set_key(:nfl, Rails.application.secrets[:sports_data_api][:key])
 puts "Sports Data API activated"
 
+# ===============================================================================
 # build NFL teams
+
 SportsDataApi::Nfl.teams.each do |team|
   Team.create!(abbr_name: team.id, market: team.market, name: team.name)
 end
 puts "NFL teams seeded"
 sleep(1)
 
-# create all NFL player profiles
+# ===============================================================================
+# build NFL profiles for offensive players
+
 offense_positions = ["QB", "WR", "RB", "TE"]
 
 Team.all.each do |team|
@@ -31,11 +40,16 @@ Team.all.each do |team|
   sleep(1)
 end
 
-# create 17 weeks
+# ===============================================================================
+# create 17 weeks for regular season
+
 1.upto(17) { |i| Week.create(week_number: i) }
 puts "Regular season weeks seeded"
 
-# create games for each week
+
+# ===============================================================================
+# update matchups / games for each week
+
 Week.all.each do |week|
   SportsDataApi::Nfl.weekly(2014, :REG, week.week_number).games.each do |g|
     away_team = g.away
@@ -48,32 +62,33 @@ Week.all.each do |week|
   sleep(1)
 end
 
+# ===============================================================================
 # update player stats for each week
+
 STAT_TYPES = [:rushing, :receiving, :passing]
 Game.all.each do |g|
   STAT_TYPES.each do |type|
-    # if g.week_id == 11
-    binding.pry
-      SportsDataApi::Nfl.player_week_stats(2014, :REG, g.week_id, g.home_team, g.away_team).stats[type].each do |s|
-        Player.all.each do |p|
-          if p.name == s[:name]
-            player = p.stats.find_or_create_by(week_id: g.week_id)
-            binding.pry
-            player.attributes.each do |k, v|
-              s.each do |item, el|
-                if k == item.to_s
-                  player[k] = s[item] if s[item].present?
-                  player.save!
-                end
+  # if g.week_id == 11
+    SportsDataApi::Nfl.player_week_stats(2014, :REG, g.week_id, g.home_team, g.away_team).stats[type].each do |s|
+      Player.all.each do |p|
+        if p.name == s[:name]
+          player = p.stats.find_or_create_by(week_id: g.week_id)
+
+          player.attributes.each do |k, v|
+            s.each do |item, el|
+              if k == item.to_s
+                player[k] = s[item] if s[item].present?
+                player.save!
               end
             end
           end
-          puts "#{p.name}'s #{type.to_s} stats updated for Week #{g.week_id}"
         end
+        puts "#{p.name}'s #{type.to_s} stats updated for Week #{g.week_id}"
       end
-    # end
-      puts "Week #{g.week_id} stats updated"
-      sleep(1)
+    end
+  # end
+    puts "Week #{g.week_id} stats updated"
+    sleep(1)
   end
 end
 
